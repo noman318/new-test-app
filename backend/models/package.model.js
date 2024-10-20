e7db1c82902c45b2b425d41b8de547541593c1d4
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2024-10-20 20:23:47 +0530

    Update in package schema added validations.

--- backend/models/package.model.js
+++ backend/models/package.model.js
@@ -3,7 +3,7 @@ const detailsSchema = mongoose.Schema({
   detail_price: { type: String, required: true },
   detail_title: { type: String, required: true },
   detail_description: { type: String, required: true },
-  images: { type: String },
+  images: { type: String, required: true },
 });
 const itenarySchema = mongoose.Schema({
   title: { type: String, required: true },
@@ -14,33 +14,41 @@ const packageSchema = new mongoose.Schema(
   {
     title: {
       type: String,
+      required: true,
     },
     description: {
       type: String,
+      required: true,
     },
     thumbnail_image: {
       type: [String],
+      required: true,
     },
     price: {
       type: Number,
+      required: true,
     },
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: false,
+      required: true,
     },
     category: {
       type: String,
+      required: true,
     },
     details: [detailsSchema],
     pickUp: {
       type: String,
+      required: true,
     },
     drop: {
       type: String,
+      required: true,
     },
     duration: {
       type: String,
+      required: true,
     },
     itinerary: [itenarySchema],
     inclusionDetail: {
